d3fc3868dcc41be0d2dfa2b232e055269e4beea1
alexyhb <fyhzjut@163.com>
2020-01-03 13:36:23 +0100

    some mac gcc bug

--- src/fact.s
+++ src/fact.s
@@ -4,11 +4,11 @@
 
 fact:
 	movq 	4(%rsp), %rcx
-	movq 	$1, %rax
-	cmpq	%rax, %rcx
+	movq 	$1 , %rax
+	cmpq	%rax , %rcx
 	jl	l7
 l1:	mulq 	%rcx
 	loop 	l1
 	jmp	l8
-l7:	movq	$1, %rcx
+l7:	movq	$1 , %rcx
 l8:	ret
