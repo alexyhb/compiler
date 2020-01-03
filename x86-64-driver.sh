df7e876f614f4d2aae2bec5050f83e7e014ae14b
alexyhb <fyhzjut@163.com>
2020-01-03 19:15:16 +0100

    driver

--- x86-64-driver.sh
+++ x86-64-driver.sh
@@ -1,25 +1,73 @@
-#!/bin/bash
+#File -o $fileName lib/lib.a#!/bin/bash
+#
+#
+#if [ $# -le 0 ]; then
+#	echo "ERROR: .calc-file input needed."
+#	exit
+#fi
+#var=${@:${#@}}
+#flag=`echo $var|cut -f2 -d "."`
+#if [ $flag != "calc" ]; then
+#	echo "ERROR: .calc-file input needed."
+#	exit
+#fi
+#
+#fileName=`echo $var|cut -f1 -d "."`
+#echo $fileName
+#assemblyFile=$fileName.s
+#
+#
+#cat prologue.s > $assemblyFile
+#./bin/calc3i < $1 >> $assemblyFile
+#cat epilogue.s >> $assemblyFile
+#
+##gcc  -shared -fPIC $assemblyFile   $fileName
+#gcc -g $assembly
+!/bin/bash
 
-
-if [ $# -le 0 ]; then
-	echo "ERROR: .calc-file input needed."
-	exit
+if [ "$#" -ne 1 ]; then
+	echo "Usage: x86-64-driver.sh <filename>"
+	exit 1
 fi
-var=${@:${#@}}
-flag=`echo $var|cut -f2 -d "."`
-if [ $flag != "calc" ]; then
-	echo "ERROR: .calc-file input needed."
-	exit
+
+echo $1 | grep -q  ".calc$" > /dev/null
+if [[ $? != 0 ]]; then
+	echo "Usage: x86-64-driver.sh <filename>"
+	echo "The file should be named with the ’.calc’ ending!!!"
+	exit 1
 fi
 
-fileName=`echo $var|cut -f1 -d "."`
-echo $fileName
-assemblyFile=$fileName.s
+FILE_DIR=$1
+FILE_NAME=$(echo $1 | sed -e 's/.*\/\([a-zA-Z0-9]*\).*/\1/g')
+ASM_FILE_NAME=${FILE_NAME}.s
 
+ASM_CODE=$(./bin/calc3i < $FILE_DIR)
+EXLIB=".global fact\n.global gcd\n.global lntwo"
+SECTION=".section"
+DATA=${SECTION}" .data"
+TEXT=${SECTION}" .text"
+BSS=".bss"
+GLOBAL_MAIN=".global main\n"
+MAIN="main:"
+PRINT_INT="print_int:\n\t.asciz\t\"%d\\\n\""
 
-cat prologue.s > $assemblyFile
-./bin/calc3i < $1 >> $assemblyFile
-cat epilogue.s >> $assemblyFile
+PRE="\tmovq\t\$0, %rbp\n\tpushq\t%rbp\n\tmovq\t%rsp, %rbp\n\tmovq\t%rax, %rdi\n"
+SUF="\txor\t%rax, %rax\n\tpopq\t%rbp\n\tcall\texit\n"
 
-#gcc  -shared -fPIC $assemblyFile   $fileName
-gcc -g $assemblyFile -o $fileName lib/lib.a
+echo "$DATA" 			>  $ASM_FILE_NAME
+echo -e "$PRINT_INT\n" 	>> $ASM_FILE_NAME
+echo "$TEXT" 			>> $ASM_FILE_NAME
+echo -e "$EXLIB\n"		>> $ASM_FILE_NAME
+echo -e "$GLOBAL_MAIN"	>> $ASM_FILE_NAME
+echo "$MAIN"			>> $ASM_FILE_NAME
+echo -e "$PRE"			>> $ASM_FILE_NAME
+echo "$ASM_CODE"		>> $ASM_FILE_NAME
+echo -e "$SUF"			>> $ASM_FILE_NAME
+echo -e "$BSS\n"		>> $ASM_FILE_NAME
+for var in {a..z};
+do
+	echo ".global $var"	 >> $ASM_FILE_NAME
+	echo "$var: .quad 0" >> $ASM_FILE_NAME
+done
+
+gcc -g $ASM_FILE_NAME -o $FILE_NAME lib/lib.a
